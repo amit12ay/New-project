ce9144d48badf98714d2649096e7693fbadbcea9
amitay7096 <51710817+amit12ay@users.noreply.github.com>
2023-01-09 15:21:44 +0545

    process

--- project/src/App.js
+++ project/src/App.js
@@ -1,26 +1,75 @@
+import React, { Children } from 'react';
 
-import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
+import {createBrowserRouter, RouterProvider, Route, Outlet} from "react-router-dom";
+import Footer from './component/Footer';
+import Navbar from './component/Navbar';
+
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+
+
+import Register from './pages/Register';
+import "./style.scss";
+
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    
+    </>
+  )
+};
 
 const router = createBrowserRouter ([
   {
     path: "/",
-    element: <div>This Is home</div>
+    element: <Layout/>,
+    Children:  [
+      {
+
+      path:"/",
+      element:<Home/>
+      },
+      {
+
+        path:"/single",
+        element:<Single/>
+        },
+        {
+
+          path:"/write",
+          element:<Write/>
+          },
+     ]
+  
+  },
+  {
+    path: "/login",
+    element: <Login/>,
 
   },
   {
-    path: "/test",
-    element: <div>This Is test</div>
+    path: "/register",
+    element: <Register/>,
 
   },
-])
+]);
 
 function App() {
   return (
-   <>
-   <div>
+  
+   <div className='app'>
+    <div className='container'>
 <RouterProvider router={router}/>
    </div>
-   </>
+   </div>
+   
   
   )
 }
